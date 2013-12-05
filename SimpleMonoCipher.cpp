52f114dc06f2523a5afe4b1898745b75111dd060
Your Name <jackiegleason@gmail.com>
2013-12-04 21:11:02 -0500

    Fixing some chars

--- SimpleMonoCipher.cpp
+++ SimpleMonoCipher.cpp
@@ -3,14 +3,22 @@ int charOffset=2;
 
 static int ALocation = 65;
 static int ZLocation = 90;
-static int aLocation = 95;
-static int zLocation = 120; 
+static int aLocation = 97;
+static int zLocation = 122; 
 
 char incrementLetter(char *clean){
   if(*clean < ALocation || (ZLocation < *clean && *clean < aLocation) || *clean>zLocation ){
     return 0;
   }
   char after = *clean + charOffset;
+  int overlap = *clean - (ZLocation - charOffset);
+  if(overlap > 0 && *clean < aLocation){
+    after = ALocation + overlap - 1;
+  }
+  int overlap2 = *clean - (zLocation - charOffset);
+  if(*clean > (zLocation - charOffset)){
+    after = aLocation + overlap2 -1;  
+  }
   return after;
 }
 
@@ -19,6 +27,14 @@ char decrementLetter(char *clean){
     return 0;
   }
   char after = *clean - charOffset;
+  if(*clean < ALocation + charOffset){
+    int overlap = *clean - (ALocation + charOffset);
+    after = ZLocation + overlap+1;
+  }
+  else if(*clean > ZLocation && *clean < (aLocation + charOffset)){
+    int overlap = *clean - (aLocation + charOffset);
+    after = zLocation + overlap + 1;
+  }
   return after;
 }
 
